21a9ba0f408b9eb373d8a2b0fd0ca953ebd0afd2
Brahms Raymundo <34291921+brahmsr@users.noreply.github.com>
2024-07-21 21:54:03 -0300

    Update Program.cs

--- ApiCrudClientes/Program.cs
+++ ApiCrudClientes/Program.cs
@@ -5,7 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
